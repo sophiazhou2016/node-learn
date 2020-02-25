e570fca0f96190545bb1fdfb9b76fce96bdb91be
zhourongjing <zhourongjing@ppdai.com>
2020-02-25 22:20:05 +0800

    使用promise的方式  读取文件夹

--- promise-test/index.js
+++ promise-test/index.js
@@ -4,27 +4,58 @@ const path = require('path')
 
 
 // callback 方式获取一个文件的内容
-function getFileContent(filename, callback) {
-    const fullFileName = path.resolve(__dirname, 'files', filename)
-    // 读取文件，异步
-    fs.readFile(fullFileName, (err, data) => {
-        if(err) {
-            console.log(err)
-            return
-        }
-        callback(
-            JSON.parse(data.toString())
-        )
-    })
-}
+// function getFileContent(filename, callback) {
+//     const fullFileName = path.resolve(__dirname, 'files', filename)
+//     // 读取文件，异步
+//     fs.readFile(fullFileName, (err, data) => {
+//         if(err) {
+//             console.log(err)
+//             return
+//         }
+//         callback(
+//             JSON.parse(data.toString())
+//         )
+//     })
+// }
 
-// 测试 callback-hell
-getFileContent('a.json', aData => {
-    console.log('aData:', aData)
-    getFileContent(aData.next, bData => {
-        console.log('bData:', bData)
-        getFileContent(bData.next, cData => {
-            console.log('cData:', cData)
+// // 测试 callback-hell
+// getFileContent('a.json', aData => {
+//     console.log('aData:', aData)
+//     getFileContent(aData.next, bData => {
+//         console.log('bData:', bData)
+//         getFileContent(bData.next, cData => {
+//             console.log('cData:', cData)
+//         })
+//     })
+// })
+
+// 用promise获取文件内容
+function getFileContent(filename) {
+    const promise = new Promise((resolve, reject) => {
+        const fullFileName = path.resolve(__dirname, 'files', filename)
+        // 读取文件，异步
+        fs.readFile(fullFileName, (err, data) => {
+            if(err) {
+                reject(err)
+                return
+            }
+            resolve(
+                JSON.parse(data.toString())
+            )
         })
     })
+    return promise
+}
+
+getFileContent('a.json').then(aData => {
+    console.log('aData', aData)
+    return getFileContent(aData.next)
+}).then(bData => {
+    console.log('bData', bData)
+    return getFileContent(bData.next)
+}).then(cData => {
+    console.log('cData', cData)
 })
+
+// async await
+// koa2
