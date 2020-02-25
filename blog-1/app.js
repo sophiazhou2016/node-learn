5f7f96057756157129903e3df893b04399c274a8
zhourongjing <zhourongjing@ppdai.com>
2020-02-25 22:40:46 +0800

    处理postData

--- blog-1/app.js
+++ blog-1/app.js
@@ -1,6 +1,33 @@
 const querystring = require('querystring')
 const handleBlogRouter = require('./src/router/blog')
 const handleUserRouter = require('./src/router/user')
+
+// 用于处理 post data
+const getPostData = (req) => {
+    const promise = new Promise((resolve, reject) => {
+        if(req.method !== 'POST') {
+            resolve({})
+            return
+        }
+        if(req.headers['Content-type'] !== 'application/json') {
+            resolve({})
+            return
+        }
+        let postData = ''
+        req.on('data', chunk => {
+            postData += chunk.toString()
+        })
+        req.on('end', () => {
+            if(!postData) {
+                resolve({})
+                return
+            }
+            resolve(JSON.parse(postData))
+        })
+    })
+    return promise
+}
+
 const serverHandle = (req, res) => {
     res.setHeader('content-type', 'application/json')
 
@@ -10,28 +37,34 @@ const serverHandle = (req, res) => {
     // 解析query
     req.query = querystring.parse(url.split('?')[1])
 
-    // 处理blog路由
-    const blogData = handleBlogRouter(req, res)
-    if(blogData) {
-        res.end(
-            JSON.stringify(blogData)
-        )
-        return
-    }
+    // 处理 post data
+    getPostData(req).then(postData => {
+        req.body = postData
+        // 处理blog路由
+        const blogData = handleBlogRouter(req, res)
+        if(blogData) {
+            res.end(
+                JSON.stringify(blogData)
+            )
+            return
+        }
 
-    // 处理user路由
-    const userData = handleUserRouter(req, res)
-    if(userData) {
-        res.end(
-            JSON.stringify(userData)
-        )
-        return
-    }
+        // 处理user路由
+        const userData = handleUserRouter(req, res)
+        if(userData) {
+            res.end(
+                JSON.stringify(userData)
+            )
+            return
+        }
 
-    // 未命中路由， 返回 404
-    res.writeHead(404, {"Content-type": "text/plain"}) // 纯文本
-    res.write("404 Not Found\n")
-    res.end()
+        // 未命中路由， 返回 404
+        res.writeHead(404, {"Content-type": "text/plain"}) // 纯文本
+        res.write("404 Not Found\n")
+        res.end()
+    })
+
+    
 }
 
 module.exports = serverHandle
