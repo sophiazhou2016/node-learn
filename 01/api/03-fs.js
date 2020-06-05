2cceaf246fb5c141e6f38f07d16c24c03d74e552
zhourongjing <zhourongjing@ppdai.com>
2020-06-05 18:14:49 +0800

    解决地狱回调async await

--- 01/api/03-fs.js
+++ 01/api/03-fs.js
@@ -1,11 +1,16 @@
 const fs = require('fs')
-// const { promisify } = require('util')
+const { promisify } = require('util')
+const readFile = promisify(fs.readFile)
 
-fs.readFile('./conf.js',(err, data) => {
-    if(err) {
-        console.log('err:', err)
-    }
-    console.log(data) // Buffer
-    console.log(data.toString())
+// fs.readFile('./conf.js',(err, data) => {
+//     if(err) {
+//         console.log('err:', err)
+//     }
+//     console.log(data) // Buffer
+//     console.log(data.toString())
+// })
+
+process.nextTick(async() => {
+    const data = await readFile('./conf.js')
+    console.log('data:', data.toString())
 })
-
