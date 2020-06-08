906c1ecaeda6b3f62d1906bb4a0be1c29aa3e372
zhourongjing <zhourongjing@ppdai.com>
2020-06-08 23:05:56 +0800

    cli 工具之开始架构 与 绚丽欢迎界面

---
+++ 01/vue-auto-router-cli/bin/vax.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+const program = require('commander')
+program.version(require('../package').version)
+
+program.command('init <name>')
+    .description('init project')
+    .action(require('../lib/init'))
+
+program.parse(process.argv)
