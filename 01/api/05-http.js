76538e687804c432d3d2add007c12526f6c57c98
zhourongjing <zhourongjing@ppdai.com>
2020-06-08 17:00:49 +0800

    node 里面 http 请求

--- 01/api/05-http.js
+++ 01/api/05-http.js
@@ -12,8 +12,9 @@ const server = http.createServer((req, res) => {
 
     const { url, method } = req
     if(url === '/' && method === 'GET') {
+        // 显示一个首页
         fs.readFile('index.html', (err, data) => {
-            if(err) {
+            if(err) { 
                 res.writeHead(500, {'Content-Type': 'text/plain;charset=utf-8'})
                 res.end('500,服务器发生错误')
                 return
@@ -23,6 +24,15 @@ const server = http.createServer((req, res) => {
             res.setHeader('Content-Type', 'text/html')
             res.end(data)
         })
+    } else if (url === '/users' && method === 'GET'){
+        res.writeHead(200, {
+            'Content-Type': 'application/json'
+        })
+        res.end(JSON.stringify([{name: 'tom', age: 20}]))
+    } else {
+        res.statusCode = 404
+        res.setHeader('Content-Type', 'text/plain,charset=utf-8')
+        res.end('404，页面没有找到')
     }
     // res.end('hello node')
 })
